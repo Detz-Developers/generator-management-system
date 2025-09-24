3905068f68bc58a0d3be1e85893d81a4fae3ed76
Kusalmarsh95 <kusalmarsh95@gmail.com>
2025-09-24 20:51:22 +0530

    QA testing
    Task

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
